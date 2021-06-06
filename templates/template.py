7c6a2df23ca257a65d9d4c4d4743058b99cfc28f
Diana Hernández-Oaxaca <hoaxacadiana@gmail.com>
2021-06-06 17:45:15 -0500

    Update template

--- templates/template.py
+++ templates/template.py
@@ -5,18 +5,36 @@ VERSION
 	[N.N]
 AUTHOR
 	Diana H. Oaxaca <hoaxacadiana@gmail.com>
+DATE
+	[dd/mm/yyyy]
 DESCRIPTION
 	[Brief description of program]
 CATEGORY
-	[Class of program]
+	[Class of program: dna seq analysis for example]
 USAGE
-	[program_name][options]
-OPTIONS
+	[program_name][options/arguments]
+ARGUMENTS
 	[name] [description]
+	[name] [description]
+	example:
+	parser.add_argument("-i", "--input",
+                    metavar="path/to/file",
+                    help="Input file",
+                    required=True)
+	parser.add_argument("-o", "--output",
+                    help="Output file",
+                    required=False)
+	
+FUNCTIONS
+	[function name] [arguments]
+	[description]
 INPUT
 	[files to run program]
 OUTPUT
 	[output file name]
 EXAMPLE
 	[describe situation, input and output files]
+	
+GITHUB LINK
+	
 '''
